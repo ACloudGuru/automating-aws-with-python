f5be3a4cd7552fa69e44c0f2e65ac5ea381e292a
Robin Norwood <robin@acloud.guru>
2018-06-06 15:34:33 -0500

    Sync directory tree to s3

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,6 +1,8 @@
 import boto3
 import click
 from botocore.exceptions import ClientError
+from pathlib import Path
+import mimetypes
 
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
@@ -69,6 +71,34 @@ def setup_bucket(bucket):
     })
 
     return
+
+def upload_file(s3_bucket, path, key):
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+    
+    s3_bucket.upload_file(
+        path,
+        key,
+        ExtraArgs={
+            'ContentType': 'text/html'
+        })
+
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    "Sync contents of PATHNAME to BUCKET"
+    s3_bucket = s3.Bucket(bucket)
+
+    root = Path(pathname).expanduser().resolve()
+
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir(): handle_directory(p)
+            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+
+    handle_directory(root)
+
+
 
 if __name__ == '__main__':
     cli()
--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Webotron currently has the following features:
 - List bucket
 - List contents of a bucket
 - Create and set up bucket
+- Sync directory tree to bucket
