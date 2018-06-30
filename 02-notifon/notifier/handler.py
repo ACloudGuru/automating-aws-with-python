9f301612b570076b384623cc08ec007e9afd1c21
Robin Norwood <robin@acloud.guru>
2018-06-30 10:36:55 -0500

    Post message to slack when lambda function is triggered

--- 02-notifon/notifier/handler.py
+++ 02-notifon/notifier/handler.py
@@ -1,8 +1,12 @@
 import os
 
+import requests
+
 def post_to_slack(event, context):
     slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
-    print(slack_webhook_url)
-    print(event)
+
+    slack_message = "From {source} at {detail[StartTime]}: {detail[Description]}".format(**event)
+    data = { "text": slack_message }
+    requests.post(slack_webhook_url, json=data)
 
     return
--- README.md
+++ README.md
@@ -24,3 +24,5 @@ Notifon is a project to notify Slack users of changes to your AWS account using 
 ### Features
 
 Notifon currently has the following features:
+
+- Send notifications to Slack when cloudwatch events happen
