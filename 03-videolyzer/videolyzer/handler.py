d041963840c7b032e4869caa82f29d4c662ea3ed
Robin Norwood <robin@acloud.guru>
2018-07-09 17:19:26 -0500

    Publish job completion to SNS topic, and handle via lambda function

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,3 +1,5 @@
+import os
+
 import urllib
 
 import boto3
@@ -10,9 +12,17 @@ def start_label_detection(bucket, key):
                 'Bucket': bucket,
                 'Name': key
             }
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+            'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
         })
 
     print(response)
+
+    # while not done:
+    #   time.sleep(10)
+    #   done = check_job_status()
 
     return
 
@@ -24,5 +34,11 @@ def start_processing_video(event, context):
             urllib.parse.unquote_plus(record['s3']['object']['key'])
         )
 
+
+    return
+
+def handle_label_detection(event, context):
+
+    print(event)
 
     return
